cbfaf2145e5bf8997e0aff1e1c2d1ed7c51bac0c
Lisa Vorontcova <lisavorontcova@yandex.ru>
2023-10-15 13:46:02 +0300

    fixed dispose job

--- SmMalloc.Allocator/Runtime/DisposeArrayJob.cs
+++ SmMalloc.Allocator/Runtime/DisposeArrayJob.cs
@@ -1,12 +1,14 @@
 ﻿namespace SmMalloc.Allocator.Runtime
 {
     using Unity.Collections;
+    using Unity.Collections.LowLevel.Unsafe;
     using Unity.Jobs;
-
+    
     internal struct DisposeArrayJob<T> : IJob where T : unmanaged
     {
+        [NativeDisableContainerSafetyRestriction]
         public NativeArray<T> Array;
-        
+
         public void Execute()
         {
             CollectionHelper.Dispose(Array);
